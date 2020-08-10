53a9abadeafb6aed17bff1aef5d31806bf4f7e89
Damzwan <damian.vlaicu@gmail.com>
2020-08-10 21:13:20 +0200

    added comments to my part

--- src/Menu.java
+++ src/Menu.java
@@ -5,13 +5,17 @@ import java.util.Map;
 
 public class Menu {
     private int[] size;
-    private LinkedHashMap<String, ButtonMethod> buttonMethodMap = new LinkedHashMap<>();
     private Difficulty selectedDifficulty;
     private Mode mode;
     private Theme theme;
     private int[] gameSize = new int[]{2, 2};
     private static int height = 700;
     private static int width = 1000;
+
+    //a Linked Hashmap that is being used to link a function to every button. At the creation of a button, we fill the label with the key and the action listener with the value
+    //This way prevents code duplication and allows us to easily extend the functionality of our game
+    private LinkedHashMap<String, ButtonMethod> buttonMethodMap = new LinkedHashMap<>();
+
 
     public static void main(String[] args) {
         new Menu(width, height);
@@ -20,13 +24,17 @@ public class Menu {
 
     public Menu(int width, int height) {
         this.size = new int[]{width, height};
+
+        // set initial settings
         setSelectedDifficulty(Difficulty.EASY);
         setMode(Mode.COMPUTER);
 
+        //create Window
         JFrame f = new JFrame("Memory Game");
         f.setSize(width, height);
         JPanel mainPanel = new JPanel();
 
+        //Fill Window with components
         fillButtonMethodMap();
         mainPanel.add(createTitlePanel());
         mainPanel.add(createDifficultyPanel());
@@ -34,9 +42,11 @@ public class Menu {
         mainPanel.add(createThemePanel());
         mainPanel.add(createSizePanel());
         mainPanel.add(createButtonPanel());
+
+
         mainPanel.setLayout(new GridLayout(6, 1));
         f.add(mainPanel);
-        f.setVisible(true);
+        f.setVisible(true); //make the window visible
     }
 
     public JPanel createTitlePanel() {
@@ -50,18 +60,20 @@ public class Menu {
     public JPanel createDifficultyPanel() {
         JPanel panel = new JPanel();
         panel.add(new JLabel("Difficulty:", SwingConstants.LEFT));
-        ButtonGroup grp = new ButtonGroup();
+        ButtonGroup grp = new ButtonGroup(); //We make use of a button group so that only one Button can be selected
 
         for (Difficulty difficulty : Difficulty.values()) {
-            JRadioButton btn = new JRadioButton(difficulty.getName());
-            btn.addActionListener(actionEvent -> setSelectedDifficulty(difficulty));
-            grp.add(btn);
-            panel.add(btn);
+            JRadioButton btn = new JRadioButton(difficulty.getName()); //Create the RadioButton with as title the name of the difficulty
+            btn.addActionListener(actionEvent -> setSelectedDifficulty(difficulty)); //add an onclick action listener
+
+            grp.add(btn); //add the button to the button group
+            panel.add(btn); //add it to the panel to make it visible
             if (difficulty == Difficulty.EASY) btn.setSelected(true);
         }
         return panel;
     }
 
+    //Same as createDifficultyPanel() --> TODO perhaps generalize for cleaner code
     public JPanel createThemePanel(){
         JPanel panel = new JPanel();
         panel.setSize(width, height/7);
@@ -77,7 +89,7 @@ public class Menu {
         return panel;
     }
 
-    //TODO perhaps merge with difficulty panel
+    //Same as createDifficultyPanel() --> TODO perhaps generalize for cleaner code
     public JPanel createModePanel() {
         JPanel panel = new JPanel();
         panel.add(new JLabel("Select your opponent:", SwingConstants.LEFT));
@@ -96,15 +108,16 @@ public class Menu {
 
     public JPanel createSizePanel() {
         JPanel panel = new JPanel();
-        SpinnerModel rowValue = new SpinnerNumberModel(3, 1, 4, 1);
-        SpinnerModel colValue = new SpinnerNumberModel(4, 1, 4, 1);
+        SpinnerModel rowValue = new SpinnerNumberModel(3, 1, 4, 1); //all possible row values
+        SpinnerModel colValue = new SpinnerNumberModel(4, 1, 4, 1); //all possible column values
 
-        //TODO code duplication
+        //add a row spinner
         panel.add(new JLabel("Rows:", SwingConstants.LEFT));
         JSpinner rowSpinner = new JSpinner(rowValue);
         rowSpinner.addChangeListener(changeEvent -> setGameSize((Integer) rowSpinner.getValue(), gameSize[1]));
         panel.add(rowSpinner);
 
+        //add a column spinner
         panel.add(new JLabel("Cols:", SwingConstants.LEFT));
         JSpinner colSpinner = new JSpinner(colValue);
         colSpinner.addChangeListener(changeEvent -> setGameSize(gameSize[0], (Integer) colSpinner.getValue()));
@@ -137,7 +150,6 @@ public class Menu {
 
     private void setSelectedDifficulty(Difficulty selectedDifficulty) {
         this.selectedDifficulty = selectedDifficulty;
-        System.out.println(this.selectedDifficulty);
     }
 
     private void setMode(Mode mode) {
@@ -165,14 +177,16 @@ public class Menu {
     }
 
     private void startGame() {
+
+        //small check to see if the game size is valid
         if (gameSize[0] * gameSize[1] % 2 != 0) {
             System.out.println("invalid game size");
             return;
         }
-        System.out.println("should start game");
-        new Game(gameSize, mode, selectedDifficulty);
+        new Game(gameSize, mode, selectedDifficulty); //The game size is valid, we create a new game instance
     }
 
+    //an interface that is being used for the buttonMethodMap field to link an action to a string
     interface ButtonMethod {
         void execute();
     }
